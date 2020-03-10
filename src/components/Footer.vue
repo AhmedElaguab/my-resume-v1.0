f7e8f3ec5551153222e2350b42b1408ad93156f0
AhmedElaguab <elaguabahmed97@gmail.com>
2020-03-10 15:51:42 +0100

    add social links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,29 @@
 <template>
   <footer class="footer">
     <p class="copy">© 2020 Ahmed Elaguab.</p>
+    <ul>
+      <!-- eslint-disable -->
+      <li>
+        <SocialIcon icon="twitter-square" link="https://twitter.com/ahmedelaguab"></SocialIcon>
+      </li>
+      <li>
+        <SocialIcon icon="linkedin" link="https://www.linkedin.com/in/ahmedelaguab/"></SocialIcon>
+      </li>
+      <li>
+        <SocialIcon icon="github-square" link="https://github.com/AhmedElaguab/"></SocialIcon>
+      </li>
+    </ul>
   </footer>
 </template>
 
 <script>
+import SocialIcon from "./SocialIcon";
+
 export default {
-  name: "Footer"
+  name: "Footer",
+  components: {
+    SocialIcon
+  }
 };
 </script>
 
@@ -15,10 +32,18 @@ export default {
   background-color: #3f51b5;
   color: #fff;
   padding: 8px 10px 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 p.copy {
   font-size: 14px !important;
   margin: 0;
+}
+
+ul {
+  display: flex;
+  list-style: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faLinkedin,
+  faTwitterSquare,
+  faGithubSquare
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faLinkedin, faTwitterSquare, faGithubSquare);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
