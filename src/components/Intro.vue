f0278616390b1fbeae34a73bc861d1002ed14903
Ahmed Elaguab <elaguabahmed97@gmail.com>
2020-05-14 19:43:34 +0100

    Update Intro.vue

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -3,8 +3,8 @@
     <h2>Hi!</h2>
     <p>
       I'm Ahmed, a Front End Web Developer with a strong desire to learn new
-      technologies. Looking for an entry-level Front End Developer role or a Web
-      Developer Internship role.
+      technologies. Looking for a Front End Developer or a Web
+      Developer opportunity.
     </p>
     <p>
       I build websites using HTML5, CSS3, JavaScript, jQuery and Sass. I can
